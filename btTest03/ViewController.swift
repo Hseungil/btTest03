9fb7c1db95b91e239be5f9698a2f01e3eb01df8f
D7703_28 <d7703_28@d7703-28ui-imac.local>
2019-03-12 15:31:56 +0900

    change Color,

--- btTest03/ViewController.swift
+++ btTest03/ViewController.swift
@@ -14,7 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
 
-
+    @IBOutlet var mylabel: UILabel!
+    
+    
+    //action은 함수
+   
+    @IBAction func bt_pressed(_ sender: Any) {
+        mylabel.text="hello"
+    }
+    
+    @IBAction func bt_init(_ sender: Any) {
+        mylabel.text=""
+    }
+    @IBAction func change_viewColor(_ sender: Any) {
+        view.backgroundColor=UIColor.yellow
+    }
+    
+    
+    @IBAction func change_viewColor2(_ sender: Any) {
+        view.backgroundColor=UIColor.white
+    }
 }
 
